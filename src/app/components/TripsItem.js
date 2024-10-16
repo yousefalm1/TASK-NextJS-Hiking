aebc8254bddcb9fd685d32684abcea3063d4e7a8
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-10-16 12:21:26 +0300

    Consistent Deminsions In The Image Grid

--- src/app/components/TripsItem.js
+++ src/app/components/TripsItem.js
@@ -3,18 +3,17 @@ import React from "react";
 
 function TripItem({ trip }) {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8 ">
-      <div className="mx-auto cursor-pointer relative overflow-hidden rounded-lg">
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-4 mb-8">
+      <div className="mx-auto cursor-pointer relative overflow-hidden rounded-lg aspect-square">
         <div className="absolute inset-0 flex items-center justify-center bg-primary bg-opacity-90 opacity-0 hover:opacity-100 transition-opacity duration-200">
-          <div className="text-center text-wh
-          ite">
+          <div className="text-center text-white">
             <i className="fas fa-plus text-3xl"></i>
           </div>
         </div>
         <Image
-          width={500}
-          height={500}
-          className="w-full h-auto"
+          width={800}
+          height={800}
+          className="w-full h-full object-cover"
           src={trip.img}
           alt="..."
         />
