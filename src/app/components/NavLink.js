c5bab8841bb123c63f412780a6f6b82d8a1f6178
Octowl <alsaff1987@gmail.com>
2024-10-19 13:08:49 +0300

    add NavLink

---
+++ src/app/components/NavLink.js
@@ -0,0 +1,18 @@
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const activeStyle = "text-primary py-3 px-3 rounded transition duration-300"
+const inactiveStyle = "text-white hover:text-primary py-3 px-3 rounded transition duration-300"
+
+function NavLink({ href }) {
+  const pathname = usePathname();
+
+  return (
+    // Complete this component so that it changes style when the link is "active"
+    <Link href={href} />
+  )
+}
+
+export default NavLink
